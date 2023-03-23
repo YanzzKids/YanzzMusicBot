593f84989414a2855d01ec64891ba2d932f6f39b
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-23 21:41:32 +0800

    Update bot.py

--- Musikku/core/bot.py
+++ Musikku/core/bot.py
@@ -24,7 +24,6 @@ class MusikkuBot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            bot_name=config.BOT_NAME,
         )
 
     async def start(self):
