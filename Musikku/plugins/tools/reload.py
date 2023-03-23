7dab5c48ca09797062eae9680470a12a2d962511
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-23 20:40:50 +0800

    Update reload.py

--- Musikku/plugins/tools/reload.py
+++ Musikku/plugins/tools/reload.py
@@ -11,7 +11,7 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import CallbackQuery, Message
 
-from config import BANNED_USERS, MUSIC_BOT_NAME, adminlist, lyrical
+from config import BANNED_USERS, MUSIC_BOT_NAME, BOT_NAME, adminlist, lyrical
 from strings import get_command
 from Musikku import app
 from Musikku.core.call import Musikku
@@ -63,7 +63,7 @@ async def reload_admin_cache(client, message: Message, _):
 @AdminActual
 async def restartbot(client, message: Message, _):
     mystic = await message.reply_text(
-        f"Please Wait.. Restarting {MUSIC_BOT_NAME} for your chat.."
+        f"Please Wait.. Restarting {BOT_NAME} for your chat.."
     )
     await asyncio.sleep(1)
     try:
